ce91d763271a93b02b08ea00c3d962d359a8be7a
Evert Arias <evert.arias@hotmail.com>
2020-03-23 10:41:25 -0400

    Fix variable declaration order

--- src/Sequence.h
+++ src/Sequence.h
@@ -6,11 +6,10 @@ class Sequence
 private:
 
     bool _is_enabled;
+    uint8_t _press_sequences;			// The number of sequences to count.
     uint32_t _press_sequence_duration;  // Time limit of the sequence.
     uint32_t _first_press_time;         // Time when button was pressed for first time.
-    uint8_t _press_sequences;			// The number of sequences to count.
     uint32_t _short_press_count;		// Short press counter.
-
 
 public:
     Sequence(uint8_t sequences, uint32_t duration):_press_sequences(sequences), 
