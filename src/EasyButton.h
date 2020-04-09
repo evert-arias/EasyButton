5b09c49eafb4b6021aaa5641350e31f7a874f3f9
elC0mpa <gcompanionib@gmail.com>
2020-04-10 01:18:41 +0200

    Ctor modiffied

--- src/EasyButton.h
+++ src/EasyButton.h
@@ -20,7 +20,7 @@ class EasyButton: public EasyButtonBase
 	friend class EasyButtonTouch;
 
 public:
-	EasyButton(uint8_t pin, uint32_t debounce_time = 35, bool pullup_enable = true, bool active_low = true) : _pin(pin), _db_time(debounce_time), _active_low(active_low), _pu_enabled(pullup_enable), _read_type(EASYBUTTON_READ_TYPE_POLL)
+	EasyButton(uint8_t pin, uint32_t debounce_time = 35, bool pullup_enable = true, bool active_low = true) : _pin(pin), _db_time(debounce_time), EasyButtonBase(active_low), _pu_enabled(pullup_enable), _read_type(EASYBUTTON_READ_TYPE_POLL)
 	{
 	}
 	~EasyButton() {}
