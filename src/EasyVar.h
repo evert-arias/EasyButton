1b1292fc0bb7bb199f9cd3656764e4330392ddcf
elC0mpa <gcompanionib@gmail.com>
2020-04-10 02:04:15 +0200

    EasyVar class draft

---
+++ src/EasyVar.h
@@ -0,0 +1,19 @@
+#ifndef _EasyVar_h
+#define _EasyVar_h
+#endif
+
+#include <Arduino.h>
+#include "EasyButtonBase.h"
+
+class EasyVar: public EasyButtonBase
+{
+private:
+    bool _button_abstraction;
+public:
+    EasyVar(bool button_abstraction, bool active_low):_button_abstraction(button_abstraction), EasyButtonBase(active_low){}
+
+    // PUBLIC FUNCTIONS
+	void begin();														// Initialize a button object and the pin it's connected to.
+	bool read();														// Returns the current debounced button state, true for pressed, false for released.
+};
+
