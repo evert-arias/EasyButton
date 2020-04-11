712f15ba01f478fb4d40369c16cf22be8ee15a20
Evert Arias <evert.arias@hotmail.com>
2020-04-11 01:41:12 -0400

    Document clean up

--- examples/Sequence/Sequence.ino
+++ examples/Sequence/Sequence.ino
@@ -1,8 +1,8 @@
 /*
- Name:		Sequence.ino
- Created:	9/5/2018 10:49:52 AM
- Author:	Evert Arias
- Description: Example to demostrate how to use the library to detect a sequence of presses on a button.
+  Name:		Sequence.ino
+  Created:	9/5/2018 10:49:52 AM
+  Author:	Evert Arias
+  Description: Example to demostrate how to use the library to detect a sequence of presses on a button.
 */
 
 #include <EasyButton.h>
@@ -14,11 +14,13 @@
 EasyButton button(BUTTON_PIN);
 
 // Callback function to be called when the button is pressed.
-void onSequenceMatched() {
-  Serial.println("Button has been pressed!");
+void onSequenceMatched()
+{
+  Serial.println("Button pressed!");
 }
 
-void setup() {
+void setup()
+{
   // Initialize Serial for debuging purposes.
   Serial.begin(115200);
   // Initialize the button.
@@ -27,7 +29,8 @@ void setup() {
   button.onSequence(5 /* number of presses */, 2000 /* timeout */, onSequenceMatched /* callback */);
 }
 
-void loop() {
-  // Continuously read the status of the button. 
+void loop()
+{
+  // Continuously read the status of the button.
   button.read();
 }
