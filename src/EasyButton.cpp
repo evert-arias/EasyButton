87acfd55da91e960cac5370561028765f587e09c
Evert Arias <evert.arias@hotmail.com>
2020-04-11 01:03:46 -0400

    Update version and document clean up

--- src/EasyButton.cpp
+++ src/EasyButton.cpp
@@ -1,7 +1,7 @@
 /**
  * EasyButton.cpp
  * @author Evert Arias
- * @version 1.1.0
+ * @version 2.0.0
  * @license MIT
  */
 
@@ -12,7 +12,9 @@ void EasyButton::begin()
 	pinMode(_pin, _pu_enabled ? INPUT_PULLUP : INPUT);
 	_current_state = _readPin();
 	if (_active_low)
+	{
 		_current_state = !_current_state;
+	}
 	_time = millis();
 	_last_state = _current_state;
 	_changed = false;
@@ -26,21 +28,26 @@ bool EasyButton::read()
 	bool pinVal = _readPin();
 
 	if (_active_low)
+	{
 		pinVal = !pinVal;
+	}
 
 	if (read_started_ms - _last_change < _db_time)
-	{ //true -> debounce time has not ellapsed
+	{
+		// Debounce time has not ellapsed.
 		_changed = false;
 	}
 	else
-	{												//true -> debounce time ellapsed
-		_last_state = _current_state;				// save last state.
-		_current_state = pinVal;					// assign new state as current state from pin's value.
-		_changed = (_current_state != _last_state); // report state change if current state vary from last state.
-		// if state has changed since last read.
+	{
+		// Debounce time ellapsed.
+		_last_state = _current_state;				// Save last state.
+		_current_state = pinVal;					// Assign new state as current state from pin's value.
+		_changed = (_current_state != _last_state); // Report state change if current state vary from last state.
+		// If state has changed since last read.
 		if (_changed)
-		{ // state change
-			// save current millis as last change time.
+		{
+			// State changed.
+			// Save current millis as last change time.
 			_last_change = read_started_ms;
 		}
 	}
@@ -50,7 +57,9 @@ bool EasyButton::read()
 		if (!_was_btn_held)
 		{
 			if (_pressed_callback)
+			{
 				_pressed_callback();
+			}
 
 			for (size_t i = 0; i < MAX_SEQUENCES; i++)
 			{
@@ -61,16 +70,19 @@ bool EasyButton::read()
 				}
 			}
 		}
-		// button was not held.
+		// Button was not held.
 		else
 		{
 			_was_btn_held = false;
 		}
-		// since button released, reset _pressed_for_callbackCalled value.
+
+		// Since button released, reset _pressed_for_callbackCalled value.
 		_held_callback_called = false;
 	}
 	else if (isPressed() && _read_type == EASYBUTTON_READ_TYPE_POLL)
+	{
 		_checkPressedTime();
+	}
 
 	_time = read_started_ms;
 
@@ -102,5 +114,7 @@ void EasyButton::disableInterrupt()
 void EasyButton::update()
 {
 	if (!_was_btn_held)
+	{
 		_checkPressedTime();
+	}
 }
--- src/EasyButton.h
+++ src/EasyButton.h
@@ -1,7 +1,7 @@
 /**
  * EasyButton.h
  * @author Evert Arias
- * @version 1.1.0
+ * @version 2.0.0
  * @license MIT
  */
 
@@ -14,8 +14,7 @@
 #define EASYBUTTON_READ_TYPE_INTERRUPT 0
 #define EASYBUTTON_READ_TYPE_POLL 1
 
-
-class EasyButton: public EasyButtonBase
+class EasyButton : public EasyButtonBase
 {
 	friend class EasyButtonTouch;
 
@@ -24,24 +23,23 @@ public:
 	{
 	}
 	~EasyButton() {}
+
 	// PUBLIC FUNCTIONS
-	virtual void begin();														// Initialize a button object and the pin it's connected to.
-	bool read();																// Returns the current debounced button state, true for pressed, false for released.
-	void update();																// Update button pressed time, only needed when using interrupts
-	void enableInterrupt(callback_t callback);									// Call a callback function when the button is pressed or released
+	virtual void begin();					   // Initialize a button object and the pin it's connected to.
+	bool read();							   // Returns the current debounced button state, true for pressed, false for released.
+	void update();							   // Update button pressed time, only needed when using interrupts.
+	void enableInterrupt(callback_t callback); // Call a callback function when the button is pressed or released.
 	void disableInterrupt();
-	bool supportsInterrupt();			 // Returns true if the button pin is an external interrupt pin
+	bool supportsInterrupt(); // Returns true if the button pin is an external interrupt pin.
 
 private:
 	// PRIVATE VARIABLES
-	uint8_t _pin;				// Arduino pin number where the Button is connected to.
-	uint32_t _db_time;			// Debounce time (ms).
-	bool _pu_enabled;			// Internal pullup resistor enabled.
-	uint8_t _read_type;			// Read type. Poll or Interrupt
+	uint8_t _pin;		// Arduino pin number where the Button is connected to.
+	uint32_t _db_time;	// Debounce time (ms).
+	bool _pu_enabled;	// Internal pullup resistor enabled.
+	uint8_t _read_type; // Read type. Poll or Interrupt.
 
-
-	virtual bool _readPin();  // Abstracts the pin value reading.
-	
+	virtual bool _readPin(); // Abstracts the pin value reading.
 };
 
 #endif
